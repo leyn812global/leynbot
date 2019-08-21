995923f9f5fc050d54ef0ec60556d6a897ea5af9
Leyn812Global <leyn727global@gmail.com>
2019-08-22 05:26:31 +0600

    First Version

---
+++ bot.py
@@ -0,0 +1,37 @@
+# Импортируем нужные компоненты 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import settings
+
+# Будем записывать отчёт о работе бота
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    logging.info(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, update.message.chat.id,
+                update.message.chat.text)
+    update.message.reply_text(user_text)
+
+# Функция, которая соединяется с платформой Telegram, "тело" нашего бота
+def main():
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+
+    logging.info('Бот запускается')
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+# Вызываем функцию - эта строчка собственно запускает бота
+main()
